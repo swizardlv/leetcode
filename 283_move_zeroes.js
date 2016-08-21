152af930ea477f01cd9f39f941a881b1a1357ff8
swizardlv <swizardlv@gmail.com>
2016-08-21 10:32:06 +0800

    283 complete

--- 283_move_zeroes.js
+++ 283_move_zeroes.js
@@ -8,10 +8,23 @@ You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 
  */
- /**
-  * @param {number[]} nums
-  * @return {void} Do not return anything, modify nums in-place instead.
-  */
- var moveZeroes = function(nums) {
-
- };
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    var len = nums.length;
+    var _count = 0;
+    for (var i = 0; i < len; i++) {
+        var _number = nums[i];
+        if (_number === 0) {
+            _count++;
+            continue;
+        }
+        if (_count > 0) {
+            var _tmp = nums[i - _count];
+            nums[i - _count] = _number;
+            nums[i] = _tmp;
+        }
+    }
+};
