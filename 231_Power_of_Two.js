23688f47bd0a5d4ccb3493b390a75be651586ce3
swizardlv <swizardlv@gmail.com>
2016-08-28 18:00:28 +0800

    231 done

---
+++ 231_Power_of_Two.js
@@ -0,0 +1,18 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+ */
+
+var isPowerOfTwo = function(n) {
+    if (n === 0) {
+        return false;
+    }
+    if (n === 1) {
+        return true;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 1) {
+        return false;
+    } else return (n / 2);
+};
