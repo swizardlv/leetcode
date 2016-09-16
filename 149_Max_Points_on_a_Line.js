06298ac8ca6691183c7a35544da8467a3c80a1f7
swizardlv <swizardlv@gmail.com>
2016-09-16 16:59:15 +0800

    149 not work

--- 149_Max_Points_on_a_Line.js
+++ 149_Max_Points_on_a_Line.js
@@ -18,16 +18,63 @@ var maxPoints = function(points) {
     if (pointsLength < 3) {
         return pointsLength;
     } else {
+        var max = 2;
         for (var i = 0; i < pointsLength - 2; i++) {
-
             var firstPoint = points[i];
-            for (var j = i + 1; i < pointsLength - 1; j++) {
+            var rateObj = {};
+            for (var j = i + 1; j < pointsLength - 1; j++) {
                 var secondPoint = points[j];
-                for (var k = j + 1; i < pointsLength; k++) {
-                    var thirdPoint = points[i];
+                var count = 2;
+                var rate;
+                if (secondPoint.x == firstPoint.x) {
+                    if (secondPoint.y == firstPoint.y) {
+                        count++;
+                        rate = 1000;
+                        if (count > max) {
+                            max = count;
 
+                        }
+                        continue;
+                    } else {
+                        rate = 999;
+                    }
+
+                } else {
+                    rate = (secondPoint.y - firstPoint.y) / (secondPoint.x - firstPoint.x);
+                }
+                if (rateObj[rate]) {
+                    continue;
+                } else {
+                    rateObj[rate] = true;
+                }
+                for (var k = j + 1; k < pointsLength; k++) {
+                    var thirdPoint = points[k];
+                    var rate1;
+                    if (thirdPoint.x == firstPoint.x) {
+                        if (thirdPoint.y == firstPoint.y) {
+
+                            count++;
+                            if (count > max) {
+                                max = count;
+
+                            }
+                            continue;
+                        } else {
+                            rate1 = 999;
+                        }
+
+                    } else {
+                        rate1 = (thirdPoint.y - firstPoint.y) / (thirdPoint.x - firstPoint.x);
+                    }
+                    if (rate1 == rate) {
+                        count++;
+                        if (count > max) {
+                            max = count;
+                        }
+                    }
                 }
             }
         }
+        return max;
     }
 };
