204ef1c4e2183386e46ab2f2064ec79cbc74b332
swizardlv <swizardlv@gmail.com>
2016-09-17 16:53:30 +0800

    149 done

--- 149_Max_Points_on_a_Line.js
+++ 149_Max_Points_on_a_Line.js
@@ -19,60 +19,39 @@ var maxPoints = function(points) {
         return pointsLength;
     } else {
         var max = 2;
-        for (var i = 0; i < pointsLength - 2; i++) {
+        for (var i = 0; i < pointsLength; i++) {
             var firstPoint = points[i];
             var rateObj = {};
-            for (var j = i + 1; j < pointsLength - 1; j++) {
-                var secondPoint = points[j];
-                var count = 2;
-                var rate;
-                if (secondPoint.x == firstPoint.x) {
-                    if (secondPoint.y == firstPoint.y) {
-                        count++;
-                        rate = 1000;
-                        if (count > max) {
-                            max = count;
-
-                        }
-                        continue;
-                    } else {
-                        rate = 999;
-                    }
-
-                } else {
-                    rate = (secondPoint.y - firstPoint.y) / (secondPoint.x - firstPoint.x);
-                }
-                if (rateObj[rate]) {
-                    continue;
-                } else {
-                    rateObj[rate] = true;
-                }
-                for (var k = j + 1; k < pointsLength; k++) {
-                    var thirdPoint = points[k];
-                    var rate1;
-                    if (thirdPoint.x == firstPoint.x) {
-                        if (thirdPoint.y == firstPoint.y) {
-
-                            count++;
-                            if (count > max) {
-                                max = count;
-
-                            }
+            var samePoint = 0;
+            var maxRate = 0;
+            for (var j = 0; j < pointsLength; j++) {
+                if (i != j) {
+                    var secondPoint = points[j];
+                    var rate;
+                    if (secondPoint.x == firstPoint.x) {
+                        if (secondPoint.y == firstPoint.y) {
+                            samePoint++;
                             continue;
                         } else {
-                            rate1 = 999;
+                            rate = 999;
                         }
-
                     } else {
-                        rate1 = (thirdPoint.y - firstPoint.y) / (thirdPoint.x - firstPoint.x);
+                        rate = (secondPoint.y - firstPoint.y) / (secondPoint.x - firstPoint.x);
                     }
-                    if (rate1 == rate) {
-                        count++;
-                        if (count > max) {
-                            max = count;
-                        }
+                    if (rateObj[rate]) {
+                        rateObj[rate]++;
+                    } else {
+                        rateObj[rate] = 1;
+                    }
+
+                    if (rateObj[rate] > maxRate) {
+                        maxRate = rateObj[rate];
                     }
                 }
+
+            }
+            if (maxRate + samePoint + 1 > max) {
+                max = maxRate + samePoint + 1;
             }
         }
         return max;
